5b28214172e289bf050990f5bc49f2f94f9d2ef2
Thomas Berrens <thomas.berrens@me.com>
2023-04-28 16:27:49 +0200

    made larger shadowbox for the cards

--- src/views/PlayerView.vue
+++ src/views/PlayerView.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="flexbox flex-col md:flex-row">
     <div class="md:w-1/4 p-4">
-      <div class="bg-gray-200 rounded-lg shadow-md overflow-hidden">
+      <div class="bg-gray-200 rounded-lg shadow-lg overflow-hidden">
         <div class="p-4">
           <h2 class="font-bold text-lg mb-2 text-red-600">{{ playerInfo.username }}</h2>
           <p class="text-gray-700 text-sm mb-2">Player ID: {{ playerInfo.uuid }}</p>
@@ -11,7 +11,7 @@
     </div>
     <div class="md:w-3/4 p-4">
       <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-        <div v-for="(gameInfo, index) in playerInfo.gamesPlayed" :key="index" class="bg-gray-200 rounded-lg shadow-md overflow-hidden">
+        <div v-for="(gameInfo, index) in playerInfo.gamesPlayed" :key="index" class="bg-gray-200 rounded-lg shadow-lg overflow-hidden">
           <div class="p-4">
             <h2 class="font-bold text-lg mb-2">Game ID: {{ gameInfo.gameID }}</h2>
             <ChoiceGraph :choices="gameInfo.choices" />
