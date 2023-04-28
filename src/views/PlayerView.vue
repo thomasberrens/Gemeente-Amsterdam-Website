4bebd9f28615b0ff77a8103b45f5abd15f08392c
Thomas Berrens <thomas.berrens@me.com>
2023-04-28 16:24:19 +0200

    changed background color of the cards in the playerview

--- src/views/PlayerView.vue
+++ src/views/PlayerView.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="flexbox flex-col md:flex-row">
     <div class="md:w-1/4 p-4">
-      <div class="bg-white rounded-lg shadow-md overflow-hidden">
+      <div class="bg-gray-200 rounded-lg shadow-md overflow-hidden">
         <div class="p-4">
-          <h2 class="font-bold text-lg mb-2">{{ playerInfo.username }}</h2>
+          <h2 class="font-bold text-lg mb-2 text-red-600">{{ playerInfo.username }}</h2>
           <p class="text-gray-700 text-sm mb-2">Player ID: {{ playerInfo.uuid }}</p>
           <p class="text-gray-700 text-sm mb-2">{{ playerInfo.gamesPlayed.length }} games played</p>
         </div>
@@ -11,7 +11,7 @@
     </div>
     <div class="md:w-3/4 p-4">
       <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-        <div v-for="(gameInfo, index) in playerInfo.gamesPlayed" :key="index" class="bg-white rounded-lg shadow-md overflow-hidden">
+        <div v-for="(gameInfo, index) in playerInfo.gamesPlayed" :key="index" class="bg-gray-200 rounded-lg shadow-md overflow-hidden">
           <div class="p-4">
             <h2 class="font-bold text-lg mb-2">Game ID: {{ gameInfo.gameID }}</h2>
             <ChoiceGraph :choices="gameInfo.choices" />
