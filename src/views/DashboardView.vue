adc72c611a6ed288cd1696f68e59b87af01c4bf7
Thomas Berrens <thomas.berrens@me.com>
2023-04-29 13:59:54 +0200

    styled & made search bar button

--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -3,20 +3,23 @@
     <div class="max-w-7xl mx-auto">
       <h1 class="text-4xl font-bold mb-4 dashboardTitle">Gemeente Amsterdam Dashboard</h1>
 
-      <div class="flex justify-between mb-4">
-        <button class="bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4 rounded shadow-lg transition-colors duration-300" @click="showCreatePlayerForm = true">
-          Create Player
-        </button>
-        <div class="flex items-center w-full max-w-xs bg-white border border-gray-300 rounded shadow-lg">
-          <div class="bg-gray-100 text-gray-500 rounded-l p-2">
+      <div class="flex flex-wrap justify-between mb-4">
+        <div class="mb-2 md:mb-0 flex-grow w-full md:w-auto">
+          <button class="bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4 rounded shadow-lg transition-colors duration-300 w-full md:w-auto" @click="showCreatePlayerForm = true">
+            Create Player
+          </button>
+        </div>
+        <div class="flex items-center w-full md:max-w-sm xs:mt-2 xs:p-2 bg-white rounded-full bg-yellow-100 shadow-md pl-2">
+          <div class="flex items-center text-gray-500">
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" class="h-4 w-4" stroke-width="2" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round">
               <circle cx="11" cy="11" r="8"></circle>
               <line x1="21" y1="21" x2="16.65" y2="16.65"></line>
             </svg>
           </div>
-          <input class="appearance-none pl-2 pr-3 py-2 text-gray-700 leading-tight focus:outline-none focus:shadow-outline w-full" type="text" placeholder="Search" v-model="searchInput" @input="filterPlayers">
+          <div class="h-5 w-px bg-gray-300 mx-1"></div>
+          <input class="appearance-none bg-transparent border-none pl-1 pr-3 py-1 text-gray-700 leading-tight focusless w-full rounded-full h-8 md:h-auto" type="text" placeholder="Search" v-model="searchInput" @input="filterPlayers">
         </div>
-      </div>
+        </div>
 
 
       <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
@@ -131,5 +134,12 @@ onBeforeMount(async () => {
 <style scoped>
 .dashboardTitle {
   color: #ec0000;
+}
+
+.focusless:focus {
+  outline: none;
+  border-color: inherit;
+  -webkit-box-shadow: none;
+  box-shadow: none;
 }
 </style>
